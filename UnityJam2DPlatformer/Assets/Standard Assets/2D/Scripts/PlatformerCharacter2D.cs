98733fd200eb90f593babc163452d1860b4933cd
Fırat Çetiner <firatcetiner@github.com>
2018-11-03 18:03:00 +0300

    flip fire point

--- UnityJam2DPlatformer/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ UnityJam2DPlatformer/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -104,10 +104,8 @@ namespace UnityStandardAssets._2D
             // Switch the way the player is labelled as facing.
             m_FacingRight = !m_FacingRight;
 
-            // Multiply the player's x local scale by -1.
-            Vector3 theScale = transform.localScale;
-            theScale.x *= -1;
-            transform.localScale = theScale;
+            /* Rotate fire point synced with player */
+            transform.Rotate(0f, 180f, 0f);
         }
     }
 }
